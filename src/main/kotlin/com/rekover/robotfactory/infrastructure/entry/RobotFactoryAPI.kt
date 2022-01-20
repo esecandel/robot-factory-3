cab3d035e827c439c15c13bcab3926f76b65b68a
scandel <sergio.candel@lastminute.com>
2022-01-20 17:03:32 +0100

    [feature] First version of RobotFactoryAPI just to accept the acceptance test (TDD)

---
+++ src/main/kotlin/com/rekover/robotfactory/infrastructure/entry/RobotFactoryAPI.kt
@@ -0,0 +1,38 @@
+package com.rekover.robotfactory.infrastructure.entry
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import java.net.URI
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class RobotFactoryAPI {
+
+    @PostMapping("/orders")
+    fun createAnOrder(@RequestBody request: OrderRequest): ResponseEntity<OrderResponse?> =
+        when (request.components) {
+            listOf("I", "A", "D", "F") -> {
+                ResponseEntity
+                    .created(URI("http://localhost:0/orders/1"))
+                    .body(OrderResponse(orderId = "1", total = 160.11f))
+            }
+            listOf("BENDER") -> {
+                ResponseEntity.badRequest().build()
+            }
+            else -> {
+                ResponseEntity.unprocessableEntity().build()
+            }
+        }
+
+
+}
+
+data class OrderRequest(val components: List<String>)
+
+data class OrderResponse(
+    @JsonProperty("order_id")
+    val orderId: String,
+    val total: Float
+)
