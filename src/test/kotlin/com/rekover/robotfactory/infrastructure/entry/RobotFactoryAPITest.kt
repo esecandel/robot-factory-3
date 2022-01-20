b405fa9d42d7d7ed4336e3c1c2a69e81cc5cc3e5
scandel <sergio.candel@lastminute.com>
2022-01-20 19:17:15 +0100

    [test] Unit test for controller

---
+++ src/test/kotlin/com/rekover/robotfactory/infrastructure/entry/RobotFactoryAPITest.kt
@@ -0,0 +1,40 @@
+package com.rekover.robotfactory.infrastructure.entry
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.http.HttpStatus
+
+internal class RobotFactoryAPITest {
+
+    private val controller = RobotFactoryAPI()
+
+    @Test
+    fun `create an order successfully`() {
+        val request = OrderRequest(listOf("I", "A", "D", "F"))
+
+        val result = controller.createAnOrder(request)
+
+        assertThat(result.statusCode).isEqualTo(HttpStatus.CREATED)
+        assertThat(result.body).isEqualTo(OrderResponse(orderId = "1", total = 160.11f))
+    }
+
+    @Test
+    fun `component not valid, return Bad Request status code`() {
+        val request = OrderRequest(listOf("BENDER"))
+
+        val result = controller.createAnOrder(request)
+
+        assertThat(result.statusCode).isEqualTo(HttpStatus.BAD_REQUEST)
+        assertThat(result.body).isNull()
+    }
+
+    @Test
+    fun `order without stock, return Unprocessable entity status code`() {
+        val request = OrderRequest(listOf("A", "C", "I", "D"))
+
+        val result = controller.createAnOrder(request)
+
+        assertThat(result.statusCode).isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY)
+        assertThat(result.body).isNull()
+    }
+}
